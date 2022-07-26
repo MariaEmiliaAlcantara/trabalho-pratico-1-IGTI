24d66383c6757b336deb30507d2b79bc29805850
MariaSchinoni <maria.schinoni10@gmail.com>
2022-07-26 14:07:20 -0300

    sorting products into different categories

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 // HTML DE CADA PRODUTO (COM INSERÇÃO DOS DETALHES)
 function productItem(product) {
   const details = loadDetails(product);
-  const item = `<div class="product" data-name=${product.name} data-brand=${product.brand} data-type=${product.product_type} tabindex="508">
-  <figure class="product-figure">
-    <img src=${product.image_link} width="215" height="215" alt=${product.name} onerror="javascript:this.src='img/unavailable.png'">
+  const item = `<div id="product" class="product" data-name=${
+    product.name.replace(/ /g, "\u00a0") || ""
+  } data-brand=${product.brand || ""} data-type=${
+    product.product_type || ""
+  }  data-price=${product.price || 0} data-rating=${
+    product.rating || 0
+  } tabindex="508">
+ <figure class="product-figure">
+    <img src=${product.image_link} width="215" height="215" alt=${
+    product.name || ""
+  } onerror="javascript:this.src='img/unavailable.png'">
   </figure>
   <section class="product-description">
-    <h1 class="product-name">${product.name}</h1>
-    <div class="product-brands"><span class="product-brand background-brand">${product.brand}</span>
-<span class="product-brand background-price">${product.price}</span></div>
+    <h1 class="product-name">${product.name || ""}</h1>
+    <div class="product-brands"><span class="product-brand background-brand">${
+      product.brand || ""
+    }</span>
+<span class="product-brand background-price">${product.price || ""}</span></div>
   </section>
   ${details}
 </div>`;
@@ -20,27 +30,37 @@ function loadDetails(product) {
   let details = `<section class="product-details"><div class="details-row">
         <div>Brand</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.brand}</div>
+          <div class="details-bar-bg" style="width= 250">${
+            product.brand || ""
+          }</div>
         </div>
       </div><div class="details-row">
         <div>Price</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.price}</div>
+          <div class="details-bar-bg" style="width= 250">${
+            product.price || 0
+          }</div>
         </div>
       </div><div class="details-row">
         <div>Rating</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.rating}</div>
+          <div class="details-bar-bg" style="width= 250">${
+            product.rating || 0
+          }</div>
         </div>
       </div><div class="details-row">
         <div>Category</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.category}</div>
+          <div class="details-bar-bg" style="width= 250">${
+            product.category || ""
+          }</div>
         </div>
       </div><div class="details-row">
         <div>Product_type</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">${product.product_type}</div>
+          <div class="details-bar-bg" style="width= 250">${
+            product.product_type || ""
+          }</div>
         </div>
       </div></section>`;
   return details;
@@ -48,7 +68,7 @@ function loadDetails(product) {
 // FETCH PRODUCTS DATA
 async function loadAllProducts() {
   const response = await fetch(
-    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl"
+    "http://makeup-api.herokuapp.com/api/v1/products.json"
   );
   const products = await response.json();
 
@@ -95,10 +115,10 @@ function onInputChangeWithDelay() {
   }, 500);
 }
 
-inputOrder.addEventListener("input", (e) => {
+/*inputOrder.addEventListener("input", (e) => {
   let inputOrderValue = e.target.value;
   return inputOrderValue;
-});
+}); */
 
 /* FILTRO POR MARCA
 inputBrand.addEventListener("input", filterByBrand);
@@ -191,5 +211,72 @@ async function filter() {
     }
 
     catalog.innerHTML = inner;
+  }
+}
+
+// ORDENAÇÃO
+inputOrder.addEventListener("input", order);
+
+function order() {
+  let array = [];
+  let div = document.querySelectorAll("#product");
+  for (let i = 0; i < div.length; i++) {
+    array.push(div[i]);
+  }
+
+  const catalog = document.getElementById("catalog");
+  let inputOrderValue = inputOrder.value;
+  let inner = "";
+
+  if (inputOrderValue === "bestRating") {
+    array.sort(compareBestRating);
+    for (item of array) {
+      inner += item.outerHTML.toString();
+    }
+    catalog.innerHTML = inner;
+  } else if (inputOrderValue === "lowerPrice") {
+    array.sort(compareLowerPrice);
+    for (item of array) {
+      inner += item.outerHTML.toString();
+    }
+    catalog.innerHTML = inner;
+  } else if (inputOrderValue === "greaterPrice") {
+    array.sort(compareGreaterPrice);
+    for (item of array) {
+      inner += item.outerHTML.toString();
+    }
+    catalog.innerHTML = inner;
+  } else if (inputOrderValue === "aToZ") {
+    array.sort(compareAToZ);
+    for (item of array) {
+      inner += item.outerHTML.toString();
+    }
+    catalog.innerHTML = inner;
+  } else if (inputOrderValue === "zToA") {
+    array.sort(compareZToA);
+    for (item of array) {
+      inner += item.outerHTML.toString();
+    }
+    catalog.innerHTML = inner;
+  }
+
+  function compareBestRating(a, b) {
+    return b.dataset.rating - a.dataset.rating;
+  }
+  function compareLowerPrice(a, b) {
+    return a.dataset.price - b.dataset.price;
+  }
+  function compareGreaterPrice(a, b) {
+    return b.dataset.price - a.dataset.price;
+  }
+  function compareAToZ(a, b) {
+    if (a.dataset.name < b.dataset.name) return -1;
+    if (a.dataset.name > b.dataset.name) return 1;
+    return 0;
+  }
+  function compareZToA(a, b) {
+    if (a.dataset.name < b.dataset.name) return 1;
+    if (a.dataset.name > b.dataset.name) return -1;
+    return 0;
   }
 }
